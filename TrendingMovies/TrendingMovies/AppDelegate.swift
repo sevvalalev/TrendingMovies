a1b795452d09e3134ef3dec69a071bcd886b1317
sevvalalev <75833556+sevvalalev@users.noreply.github.com>
2023-05-05 23:08:39 +0300

    Initial Commit

---
+++ TrendingMovies/TrendingMovies/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TrendingMovies
+//
+//  Created by Şevval Alev on 5.05.2023.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let navigationController = UINavigationController(rootViewController: MainViewController())
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        
+        self.window = window
+        return true
+    }
+}
+
---
+++ TrendingMovies/TrendingMovies/Controllers/MainViewController/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  TrendingMovies
+//
+//  Created by Şevval Alev on 5.05.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Trending Movies"
+    }
+
+
+
+}
